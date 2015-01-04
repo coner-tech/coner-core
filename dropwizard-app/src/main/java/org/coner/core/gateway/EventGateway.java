5f5f36ca69e267bb1be20d81e45e8c07d1501fcc
Carlton Whitehead <cebesius86@gmail.com>
2015-01-04 00:11:12 -0500

    Add EventGateway javadocs

--- dropwizard-app/src/main/java/org/coner/core/gateway/EventGateway.java
+++ dropwizard-app/src/main/java/org/coner/core/gateway/EventGateway.java
@@ -9,30 +9,52 @@ import org.coner.hibernate.dao.EventDao;
 import java.util.List;
 
 /**
- *
+ * EventGateway wraps persistence layer interactions for Event domain entities
  */
 public class EventGateway {
 
     private final EventBoundary eventBoundary;
     private final EventDao eventDao;
 
+    /**
+     * Constructor for EventGateway
+     *
+     * @param eventBoundary the EventBoundary for converting Domain entities to/from Hibernate entities
+     * @param eventDao      the EventDao for interacting with the persistence layer
+     */
     public EventGateway(EventBoundary eventBoundary, EventDao eventDao) {
         this.eventBoundary = eventBoundary;
         this.eventDao = eventDao;
     }
 
+    /**
+     * Get all Event entities
+     *
+     * @return a list of all Event entities
+     */
     public List<Event> getAll() {
         List<org.coner.hibernate.entity.Event> events = eventDao.findAll();
         return eventBoundary.toDomainEntities(events);
     }
 
+    /**
+     * Get an Event entity by id
+     *
+     * @param eventId eventId of the Event
+     * @return the Event entity with id or null if not found
+     */
     public Event findById(String eventId) {
         Preconditions.checkArgument(!Strings.isNullOrEmpty(eventId));
         org.coner.hibernate.entity.Event hibernateEvent = eventDao.findById(eventId);
         return eventBoundary.toDomainEntity(hibernateEvent);
     }
 
-    public void create(org.coner.core.domain.Event event) {
+    /**
+     * Persist a new Event entity
+     *
+     * @param event the Event entity to persist
+     */
+    public void create(Event event) {
         Preconditions.checkNotNull(event);
         org.coner.hibernate.entity.Event hibernateEvent = eventBoundary.toHibernateEntity(event);
         eventDao.create(hibernateEvent);
