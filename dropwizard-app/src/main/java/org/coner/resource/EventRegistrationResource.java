1d2abbaf1bccaf30ffa062d91bd551944a45e978
Carlton Whitehead <cebesius86@gmail.com>
2015-03-22 20:05:53 -0400

    Fixes #101 GET /events/{eventId}/registrations/{registrationId} error handling

--- dropwizard-app/src/main/java/org/coner/resource/EventRegistrationResource.java
+++ dropwizard-app/src/main/java/org/coner/resource/EventRegistrationResource.java
@@ -2,9 +2,11 @@ package org.coner.resource;
 
 import io.dropwizard.hibernate.UnitOfWork;
 import org.coner.api.entity.Registration;
+import org.coner.api.response.ErrorsResponse;
 import org.coner.boundary.EventBoundary;
 import org.coner.boundary.RegistrationBoundary;
 import org.coner.core.ConerCoreService;
+import org.coner.core.exception.EventRegistrationMismatchException;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -13,6 +15,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
 
 /**
  * The EventRegistrationResource exposes getting, updating, or deleting a
@@ -46,21 +50,40 @@ public class EventRegistrationResource {
     /**
      * Get a Registration.
      *
-     * @param eventId the id of the Event to get
+     * @param eventId        the id of the Event to get
      * @param registrationId the id of the Registration
      * @return the Event with the id
-     * @throws javax.ws.rs.NotFoundException if no Event is found having the id
+     * @throws javax.ws.rs.NotFoundException if no Event is found having eventId, or if no Registration is found having
+     *                                       registrationId
      */
     @GET
     @UnitOfWork
-    public Registration getRegistration(@PathParam("eventId") String eventId,
-                                        @PathParam("registrationId") String registrationId) {
-        org.coner.core.domain.Event domainEvent = conerCoreService.getEvent(eventId);
-        if (domainEvent == null) {
-            throw new NotFoundException("No event with id " + eventId);
+    public Response getRegistration(@PathParam("eventId") String eventId,
+                                    @PathParam("registrationId") String registrationId) {
+        org.coner.core.domain.Registration domainRegistration;
+        try {
+            domainRegistration = conerCoreService.getRegistration(
+                    eventId,
+                    registrationId
+            );
+        } catch (EventRegistrationMismatchException e) {
+            ErrorsResponse errorsResponse = new ErrorsResponse();
+            errorsResponse.setErrors(Arrays.asList(
+                    Response.Status.CONFLICT.getReasonPhrase(),
+                    e.getMessage()
+            ));
+            return Response.status(Response.Status.CONFLICT)
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(errorsResponse)
+                    .build();
         }
-        Registration registration = registrationBoundary.toApiEntity(
-                conerCoreService.getRegistration(registrationId));
-        return registration;
+        if (domainRegistration == null) {
+            throw new NotFoundException("No registration with id " + registrationId);
+        }
+
+        Registration registration = registrationBoundary.toApiEntity(domainRegistration);
+
+        return Response.ok(registration, MediaType.APPLICATION_JSON_TYPE)
+                .build();
     }
 }
