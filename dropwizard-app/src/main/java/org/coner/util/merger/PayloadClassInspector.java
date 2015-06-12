ce341b88691dec89afe1ba09dd2126c52ffb6d0b
Carlton Whitehead <cebesius86@gmail.com>
2015-06-11 22:05:30 -0400

    Add ReflectionPayloadJavaBeanMerger and PayloadClassInspector

---
+++ dropwizard-app/src/main/java/org/coner/util/merger/PayloadClassInspector.java
@@ -0,0 +1,37 @@
+package org.coner.util.merger;
+
+import com.google.common.collect.ImmutableMap;
+import java.lang.reflect.Field;
+
+public final class PayloadClassInspector {
+
+    private final Class<?> payloadClass;
+    private ImmutableMap<String, Field> fieldNamesToFields;
+
+    public PayloadClassInspector(Class<?> payloadClass) {
+        this.payloadClass = payloadClass;
+    }
+
+    public boolean hasFieldName(String fieldName) {
+        if (fieldNamesToFields == null) {
+            buildFieldNamesToFields();
+        }
+        return fieldNamesToFields.containsKey(fieldName);
+    }
+
+    public Field getFieldByName(String fieldName) {
+        if (fieldNamesToFields == null) {
+            buildFieldNamesToFields();
+        }
+        return fieldNamesToFields.get(fieldName);
+    }
+
+    private void buildFieldNamesToFields() {
+        ImmutableMap.Builder<String, Field> builder = ImmutableMap.builder();
+        for (Field field : payloadClass.getDeclaredFields()) {
+            builder.put(field.getName(), field);
+        }
+        fieldNamesToFields = builder.build();
+    }
+
+}
---
+++ dropwizard-app/src/test/java/org/coner/util/merger/ReflectionPayloadJavaBeanMergerTest.java
@@ -0,0 +1,105 @@
+package org.coner.util.merger;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.coner.util.merger.ReflectionPayloadJavaBeanMerger.javaBeanToPayload;
+import static org.coner.util.merger.ReflectionPayloadJavaBeanMerger.payloadToJavaBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+public class ReflectionPayloadJavaBeanMergerTest {
+
+    @Test
+    public void whenMergeSimpleJavaBeanToSimplePayloadItShouldMerge() {
+        SimpleJavaBean sourceJavaBean = new SimpleJavaBean();
+        sourceJavaBean.setTheInt(42);
+
+        SimplePayload destinationPayload = new SimplePayload();
+
+        ReflectionPayloadJavaBeanMerger<SimpleJavaBean, SimplePayload> merger = javaBeanToPayload();
+        merger.merge(sourceJavaBean, destinationPayload);
+
+        assertThat(destinationPayload.theInt).isEqualTo(42);
+    }
+
+    @Test
+    public void whenMergeSimplePayloadToSimpleJavaBeanItShouldMerge() {
+        SimplePayload sourcePayload = new SimplePayload();
+        sourcePayload.theInt = 42;
+
+        SimpleJavaBean destinationJavaBean = new SimpleJavaBean();
+
+        ReflectionPayloadJavaBeanMerger<SimplePayload, SimpleJavaBean> merger = payloadToJavaBean();
+        merger.merge(sourcePayload, destinationJavaBean);
+
+        assertThat(destinationJavaBean.getTheInt()).isEqualTo(42);
+    }
+
+    @Test
+    public void whenGivenAdditionalMergerItShouldAlwaysMerge() {
+        ObjectMerger<SimpleJavaBean, SimplePayload> additionalMerger = Mockito.mock(ObjectMerger.class);
+        SimpleJavaBean simpleJavaBean = new SimpleJavaBean();
+        SimplePayload simplePayload = new SimplePayload();
+
+        ReflectionPayloadJavaBeanMerger<SimpleJavaBean, SimplePayload> merger = javaBeanToPayload(additionalMerger);
+        merger.merge(simpleJavaBean, simplePayload);
+
+        verify(additionalMerger).merge(simpleJavaBean, simplePayload);
+        verifyNoMoreInteractions(additionalMerger);
+    }
+
+    @Test
+    public void whenMergeSimplePayloadToSlightlyDeviousJavaBeanItShouldMerge() {
+        SimplePayload sourcePayload = new SimplePayload();
+        sourcePayload.devious = true;
+        SlightlyDeviousJavaBean destinationJavaBean = new SlightlyDeviousJavaBean();
+
+        ReflectionPayloadJavaBeanMerger merger = payloadToJavaBean();
+        merger.merge(sourcePayload, destinationJavaBean);
+
+        assertThat(destinationJavaBean.getDevious()).isTrue();
+    }
+
+    public static class SimpleJavaBean {
+        private int theInt;
+
+        public int getTheInt() {
+            return theInt;
+        }
+
+        public void setTheInt(int theInt) {
+            this.theInt = theInt;
+        }
+    }
+
+    public static class SimplePayload {
+        public int theInt;
+        public boolean devious;
+    }
+
+    public static class SlightlyDeviousJavaBean {
+        private int theInt;
+        private String devious;
+
+        public int getTheInt() {
+            return theInt;
+        }
+
+        public void setTheInt(int theInt) {
+            this.theInt = theInt;
+        }
+
+        // return type doesn't match field type
+        public boolean getDevious() {
+            return Boolean.valueOf(devious);
+        }
+
+        // field type doesn't match parameter 0 type
+        public void setDevious(boolean devious) {
+            this.devious = String.valueOf(devious);
+        }
+    }
+}
