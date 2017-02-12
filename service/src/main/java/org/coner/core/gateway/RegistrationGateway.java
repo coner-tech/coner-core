7f59d1867d2537fa5a8debd63815a707858b2582
Carlton Whitehead <cebesius86@gmail.com>
2017-02-12 14:19:53 -0500

    Remove remaining Event and Registration boundaries

--- service/src/main/java/org/coner/core/gateway/RegistrationGateway.java
+++ service/src/main/java/org/coner/core/gateway/RegistrationGateway.java
@@ -4,43 +4,43 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.coner.core.boundary.EventHibernateDomainBoundary;
-import org.coner.core.boundary.RegistrationHibernateAddPayloadBoundary;
-import org.coner.core.boundary.RegistrationHibernateDomainBoundary;
 import org.coner.core.domain.entity.Event;
 import org.coner.core.domain.entity.Registration;
 import org.coner.core.domain.payload.RegistrationAddPayload;
 import org.coner.core.hibernate.dao.RegistrationDao;
 import org.coner.core.hibernate.entity.EventHibernateEntity;
 import org.coner.core.hibernate.entity.RegistrationHibernateEntity;
+import org.coner.core.mapper.EventMapper;
+import org.coner.core.mapper.RegistrationMapper;
 
 import com.google.common.base.Preconditions;
 
-public class RegistrationGateway extends AbstractGateway<
+public class RegistrationGateway extends MapStructAbstractGateway<
+        RegistrationAddPayload,
         Registration,
         RegistrationHibernateEntity,
-        RegistrationAddPayload,
-        RegistrationHibernateDomainBoundary,
-        RegistrationHibernateAddPayloadBoundary,
         RegistrationDao> {
 
-    private final EventHibernateDomainBoundary eventEntityBoundary;
+    private final RegistrationMapper registrationMapper;
+    private final EventMapper eventMapper;
 
     @Inject
-    public RegistrationGateway(
-            RegistrationHibernateDomainBoundary entityBoundary,
-            RegistrationHibernateAddPayloadBoundary addPayloadBoundary,
-            RegistrationDao dao,
-            EventHibernateDomainBoundary eventEntityBoundary) {
-        super(entityBoundary, addPayloadBoundary, dao);
-        this.eventEntityBoundary = eventEntityBoundary;
+    public RegistrationGateway(RegistrationMapper registrationMapper, EventMapper eventMapper, RegistrationDao dao) {
+        super(
+                registrationMapper::toHibernateEntity,
+                registrationMapper::toDomainEntity,
+                registrationMapper::toDomainEntities,
+                dao
+        );
+        this.registrationMapper = registrationMapper;
+        this.eventMapper = eventMapper;
     }
 
     public List<Registration> getAllWith(Event event) {
         Preconditions.checkNotNull(event);
-        EventHibernateEntity hibernateEvent = eventEntityBoundary.toLocalEntity(event);
+        EventHibernateEntity hibernateEvent = eventMapper.toHibernateEntity(event);
         List<RegistrationHibernateEntity> registrations = dao.getAllWith(hibernateEvent);
-        return entityBoundary.toRemoteEntities(registrations);
+        return registrationMapper.toDomainEntities(registrations);
     }
 
 }
